6fd5f04b51efbbab534b11beb032affc32e0d77a
achujoseph0709 <achujozefsl0709@gmail.com>
2024-11-13 23:56:07 +0530

    13-11

--- kvs_app/models.py
+++ kvs_app/models.py
@@ -318,7 +318,7 @@ class Join_Kvs(models.Model):
     district = models.CharField(max_length=50,choices=DISTRICT_CHOICES,blank=True,null=True)
     union = models.CharField(max_length=100,blank=True,null=True)
     sakha_no = models.CharField(max_length=25,blank=True,null=True)
-    id_proof = models.ForeignKey(Id_details_choices,on_delete=models.CASCADE,blank=True,null=True)
+    id_proof = models.ForeignKey(Id_details_choices, on_delete=models.SET_NULL, null=True, blank=True)
     id_proof_no = models.CharField(max_length=25,blank=True,null=True)
     payment_details = models.ForeignKey(Payment_details_choices,on_delete=models.CASCADE,blank=True,null=True)
     STATUS_CHOISES = (
